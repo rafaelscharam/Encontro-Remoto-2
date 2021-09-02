c793e292d3a8a888aadccbaf58cd7ab450017a20
Rafael Scharam <rafaelscharam@gmail.com>
2021-09-01 23:00:50 -0300

    Entrega da atividade

---
+++ index.js
@@ -0,0 +1,12 @@
+let numeroDeAlunos = 20;
+
+for (let contador = 0; contador <= numeroDeAlunos; contador ++){
+    
+    if (contador == 0){
+        console.log("O número atual é ZERO");
+    } else if (contador % 2 == 0 ){
+        console.log (`O número ${contador} é PAR`);
+    } else {
+        console.log (`O número ${contador} é ÍMPAR`);
+    }
+}
